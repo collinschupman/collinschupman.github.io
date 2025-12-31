bb5be39a35cdee65cc4c0d565d3292978b4b4881
Collin Schupman <collinschupman@gmail.com>
2025-12-30 19:11:10 -0800

    Add music: Kingling EP, Loveless Colorado single, and No Exits tracks with album artwork

--- src/components/MusicGrid.tsx
+++ src/components/MusicGrid.tsx
@@ -24,6 +24,48 @@ const music = [
     link: 'https://noexitsla.bandcamp.com/album/no-exits-ep',
     year: '2020',
   },
+  {
+    title: 'Fortune',
+    description: 'Single track from No Exits',
+    image: '/fortune.png',
+    link: 'https://noexitsla.bandcamp.com/track/fortune',
+    year: '',
+  },
+  {
+    title: 'Shapes',
+    description: 'Single track from No Exits',
+    image: '/shapes.png',
+    link: 'https://noexitsla.bandcamp.com/track/shapes',
+    year: '',
+  },
+  {
+    title: 'Catatonic',
+    description: 'Single track from No Exits',
+    image: '/catatonic.png',
+    link: 'https://noexitsla.bandcamp.com/track/catatonic',
+    year: '',
+  },
+  {
+    title: 'No Exits Demos',
+    description: 'Demo recordings from No Exits',
+    image: '/demos.png',
+    link: 'https://noexitsla.bandcamp.com/album/demos',
+    year: '',
+  },
+  {
+    title: 'Loveless Colorado: Sunrise Single',
+    description: 'Single - bass guitar contribution',
+    image: '/loveless.png',
+    link: 'https://lovelesscolorado.bandcamp.com/track/sunrise-2',
+    year: '2017',
+  },
+  {
+    title: 'Kingling',
+    description: 'Indie Rock EP - bass, guitar, and lyrics',
+    image: '/kingling.png',
+    link: 'https://kingling.bandcamp.com/album/kingling',
+    year: '2016',
+  },
 ]
 
 export default function MusicGrid() {
